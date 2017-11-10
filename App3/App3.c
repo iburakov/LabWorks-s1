1faa621892636794e9ec7f42246424b01f85061a
Ilya Burakov <speedwatson@gmail.com>
2017-11-10 14:04:16 +0300

    app3 is now work-in-progress

--- App3/App3.c
+++ App3/App3.c
@@ -1,9 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "globals.h"
+#include "input.h"
 
-int main()
-{
-	
-    return 0;
+char* get_description(int inp) {
+	switch (inp)
+	{
+	case '1': return "partdesc1"; break;
+	case '2': return "partdesc2"; break;
+	default: return "No description provided"; break;
+	}
 }
 
+int main(void)
+{
+	setlocale(LC_CTYPE, "Russian");
+	printf("Илья Бураков, вариант 3\n");
+	setlocale(LC_CTYPE, "C");
+
+	printf("Please, choose a part of task: ");
+	int inp = getchar_after_spaces();
+
+	if (inp != '1' && inp != '2' || getchar_after_spaces() != '\n') {
+		printf("Invalid part number, type '1' or '2'\n");
+		return 1;
+	} // стоит ли писать else после if'а с безусловным return'ом?
+
+	char* desc = (inp == '1') ? "partdesc1" : "partdesc2";
+	printf("Part #%c - %s", inp, desc);
+
+
+
+
+
+
+	printf("Bye!\n");
+	return 0;
+}
